e8a26a1c4ff00c07e1ad4f0079c55c48b44caad5
Marcos <marcos.vinicius.dev.2004@gmail.com>
2023-02-13 20:17:27 -0300

    Garantindo funcionamento do fluxo de execução

--- src/mainClass.java
+++ src/mainClass.java
@@ -6,11 +6,40 @@ public class mainClass {
         System.out.println("|   ADVINHE O NÚMERO    |");
         System.out.println("-------------------------");
 
+        System.out.println("\nEncontre o número aleatório secreto!!.\n");
 
-        System.out.println("\nEncontre o número aleatório secreto!!.");
-        System.out.print("Digite 's' para iniciar:");
-        
+
+        mathClass matematica = new mathClass();
+
         Scanner input = new Scanner(System.in);
-        char escolha = input.next().charAt(0);
+
+        int numTentativas = 5;
+
+        int numAleatorio = matematica.gerarNum();
+        String parImpar = matematica.verificarPar(numAleatorio);
+        String multiplo = matematica.encontrarMúltiplo(numAleatorio);
+        int numEscolhido = -1;
+
+        System.out.println("O número " + parImpar);
+        System.out.println("O número " + multiplo);
+
+        while (numTentativas > 0) {
+            System.out.println("Você tem " + numTentativas + " tentativas.");
+            numEscolhido = input.nextInt();
+
+            if (numEscolhido == numAleatorio) {
+                System.out.println("Você acertou o número!");
+                break;
+            } else {
+                System.out.println("Não foi o número certo...");
+                numTentativas--;
+            }
+        }
+
+        input.close();
+
+        if(numTentativas <= 0){
+            System.out.println("Você não conseguiu acertar o número. O número é:" + numAleatorio + ". Tente mais uma vez!");
+        }
     }
 }
