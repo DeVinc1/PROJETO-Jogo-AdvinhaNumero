217d0db1ec463f6045f721902789d416c9655f1e
Marcos <marcos.vinicius.dev.2004@gmail.com>
2023-02-13 19:01:12 -0300

    Inciando fluxo principal do app

---
+++ src/mainClass.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class mainClass {
+    public static void main(String[] args) throws Exception {
+        System.out.println("-------------------------");
+        System.out.println("|   ADVINHE O NÚMERO    |");
+        System.out.println("-------------------------");
+
+
+        System.out.println("\nEncontre o número aleatório secreto!!.");
+        System.out.print("Digite 's' para iniciar:");
+        
+        Scanner input = new Scanner(System.in);
+        char escolha = input.next().charAt(0);
+    }
+}
