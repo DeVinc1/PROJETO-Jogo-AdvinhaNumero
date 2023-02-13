22a1adc1a42d24213656f92b65ad0d09f0023201
Marcos <marcos.vinicius.dev.2004@gmail.com>
2023-02-13 20:54:00 -0300

    Refatorando e comentando código [FINALIZADO]

--- src/mainClass.java
+++ src/mainClass.java
@@ -2,27 +2,40 @@ import java.util.Scanner;
 
 public class mainClass {
     public static void main(String[] args) throws Exception {
+        /*
+         * Cabeçalho.
+         */
         System.out.println("-------------------------");
         System.out.println("|   ADVINHE O NÚMERO    |");
         System.out.println("-------------------------");
 
         System.out.println("\nEncontre o número aleatório secreto!!.\n");
 
+        /*
+         * Instâncias de classes.
+         */
 
         mathClass matematica = new mathClass();
-
         Scanner input = new Scanner(System.in);
 
+        /*
+         * Variáveis que serão usadas.
+         */
         int numTentativas = 5;
-
+        int numEscolhido = -1;
         int numAleatorio = matematica.gerarNum();
         String parImpar = matematica.verificarPar(numAleatorio);
         String multiplo = matematica.encontrarMúltiplo(numAleatorio);
-        int numEscolhido = -1;
 
-        System.out.println("O número " + parImpar);
+        /*
+         * Informações sobre o número aleatório gerado.
+         */
+        System.out.println("\nO número " + parImpar);
         System.out.println("O número " + multiplo);
 
+        /*
+         * Loop para executar cada tentativa 
+         */
         while (numTentativas > 0) {
             System.out.println("Você tem " + numTentativas + " tentativas.");
             numEscolhido = input.nextInt();
@@ -38,6 +51,9 @@ public class mainClass {
 
         input.close();
 
+        /*
+         * Mensagem em caso de derrota no jogo.
+         */
         if(numTentativas <= 0){
             System.out.println("Você não conseguiu acertar o número. O número é:" + numAleatorio + ". Tente mais uma vez!");
         }
